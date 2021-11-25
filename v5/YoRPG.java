3e4d9d1610909beabe897ff06080989832c17e4e
Tommy Chan <ychan30@stuy.edu>
2021-11-24 20:11:16 -0500

    v5

--- v5/YoRPG.java
+++ v5/YoRPG.java
@@ -207,6 +207,7 @@ public class YoRPG {
       //option 2: you slay the beast
       else if ( !smaug.isAlive() ) {
         System.out.println( "HuzzaaH! Ye olde monster hath been slain!" );
+        pat.addHP(20);
         return true;
       }
       //option 3: the beast slays you
